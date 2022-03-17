c03037102ec86b39ac084a955e3fd45f24670175
Caiodiv <84077829+Caiodiv@users.noreply.github.com>
2022-03-17 19:36:00 -0300

    Add files via upload

---
+++ Fito/Lp/6°script.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Programa PHP</title>
+
+</head>
+
+<body>
+
+<h1> Esta página é para me apresentar! </h1>
+    
+<?php
+
+$name='Sylvinho';
+$sobrenome='Silva Costa';
+$nasc= '17/09/2005';
+ 
+ echo "Meu nome é <u> <i> <b> $name </i></b> </u>";
+ echo "<br>";
+ echo "Nasci em <u> <i> <b> $nasc </i> </b> </u>";
+ echo "<br>";
+ echo "Meu sobrenome é <u> <i> <b> $sobrenome </i> </b> </u>";
+
+?>  
+</body>
+
+</html>
